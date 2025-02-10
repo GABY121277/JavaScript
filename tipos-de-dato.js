228b1d1745d4086fbf3d8333e0212ad2eca359b0
GABY121277 <gladys.gabriela0901@gmail.com>
2025-02-09 20:39:08 -0600

    "1.Tipos-datos-JavaScript"

---
+++ tipos-de-dato.js
@@ -0,0 +1,21 @@
+// Tipos de datos en JavaScript
+console.log( 'Este es mi repo de tipos de datos en JavaScript:' )
+
+console.log( "El tipo de dato 42 es:", typeof 42); // numero
+console.log("El tipo de dato es:", typeof 'Veinticinco'); // cadena
+console.log("El tipo de dato es:", typeof -666);         
+console.log("El tipo de dato es:", typeof true);// boolean
+console.log("El tipo de dato es:", typeof 0);            
+console.log("El tipo de dato es:", typeof '');// cadena
+console.log("El tipo de dato es:", typeof null);// objeto
+console.log("El tipo de dato es:", typeof undefined);// indefinido
+console.log("El tipo de dato es:", typeof FALSE);        
+
+// Otros
+console.log( 'hola mundo' )
+console.log( "El tipo de dato false es:", typeof false )
+console.log(typeof NaN);          // numero
+console.log(typeof []);           // objeto (arreglo)
+console.log(typeof function(){}); // funcion
+console.log(typeof Symbol('id')); // simbolo
+console.log(typeof BigInt(9007199254740991)); // numeros grandes
