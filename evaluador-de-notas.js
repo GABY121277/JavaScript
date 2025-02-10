933f195709e62cf10508422a321321c83d1e2e88
GABY121277 <gladys.gabriela0901@gmail.com>
2025-02-09 20:40:51 -0600

    "2.Comparaciones-y-decisiones"

---
+++ evaluador-de-notas.js
@@ -0,0 +1,13 @@
+// Comparaciones y decisiones en JavaScript
+
+let nota = 100; 
+
+if (nota >= 90) {
+    console.log("Excelente");
+} else if (nota >= 75) {
+    console.log("Bien");
+} else if (nota >= 60) {
+    console.log("Suficiente");
+} else {
+    console.log("No aprobó");
+}
